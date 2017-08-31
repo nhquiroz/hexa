cb4f1c9810edecee94420c49da13370866529a6f
Nicolás Quiroz <nhsz@users.noreply.github.com>
2017-08-31 11:42:24 -0300

    Update hexa.js

--- hexa.js
+++ hexa.js
@@ -2,24 +2,24 @@
   function Hexa() {
   }
 
-  Hexa.prototype.getRandomHexDigit = function() {
-    var hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  Hexa.prototype.getRandomHexDigit = () => {
+    let hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 		return hexDigits[Math.floor(Math.random() * 16)];
   };
 
-  Hexa.prototype.getRandomHexColor = function() {
-    var hexNumber = '#';
+  Hexa.prototype.getRandomHexColor = () => {
+    let hexNumber = '#';
     while (hexNumber.length < 7) {
       hexNumber += Hexa.prototype.getRandomHexDigit();
     }
     return hexNumber;
   };
 
-  Hexa.prototype.printRandomHexDigit = function() {
+  Hexa.prototype.printRandomHexDigit = () => {
     console.log(Hexa.prototype.getRandomHexDigit());
   };
 
-  Hexa.prototype.printRandomHexColor = function() {
+  Hexa.prototype.printRandomHexColor = () => {
     console.log(Hexa.prototype.getRandomHexColor());
   };
 
