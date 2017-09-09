a59c578526c0a438f8b327b152d6db0741216b38
Nicolás Quiroz <nhsz@users.noreply.github.com>
2017-09-08 23:44:54 -0300

    Update hexa.js

--- hexa.js
+++ hexa.js
@@ -3,25 +3,25 @@
   }
 
   Hexa.prototype.getRandomHexDigit = () => {
-    let hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
-		return hexDigits[Math.floor(Math.random() * 16)];
-  };
+    let hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+		return hexDigits[Math.floor(Math.random() * 16)]
+  }
 
   Hexa.prototype.getRandomHexColor = () => {
-    let hexNumber = '#';
+    let hexNumber = '#'
     while (hexNumber.length < 7) {
-      hexNumber += Hexa.prototype.getRandomHexDigit();
+      hexNumber += Hexa.prototype.getRandomHexDigit()
     }
-    return hexNumber;
-  };
+    return hexNumber
+  }
 
   Hexa.prototype.printRandomHexDigit = () => {
-    console.log(Hexa.prototype.getRandomHexDigit());
-  };
+    console.log(Hexa.prototype.getRandomHexDigit())
+  }
 
   Hexa.prototype.printRandomHexColor = () => {
-    console.log(Hexa.prototype.getRandomHexColor());
-  };
+    console.log(Hexa.prototype.getRandomHexColor())
+  }
 
-  window.Hexa = Hexa;
-})();
+  window.Hexa = Hexa
+})()
